ca09d74567a8c3d9c71b4be36cf49e1716e46e44
shopify-development-code <75614025+shopify-development-code@users.noreply.github.com>
2026-01-14 15:27:45 +0530

    smooth-scroller add

--- service-based/V1.0.0/animation-engine.js
+++ service-based/V1.0.0/animation-engine.js
@@ -1,1 +1,40 @@
-console.log('CDN file structure updated');
+document.fonts.ready.then(() => {
+ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 1.2,
+  effects: true
+});
+const revealTextSection = document.querySelector('[text-reveal-on-scroll]');
+if(revealTextSection){
+  const revealTextValue = revealTextSection.getAttribute("text-reveal-on-scroll");
+  const revealContentAll = revealTextSection.querySelectorAll('[reveal-content]');
+  revealContentAll.forEach(el => {
+    const split = new SplitText(el, {
+      type: "lines",
+      linesClass: "line"
+    });
+    const lines = split.lines;
+    let activeIndex = -1;
+    ScrollTrigger.create({
+      trigger: el,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+      onUpdate(self) {
+        const progress = self.progress;
+        const index = Math.min(
+          lines.length - 1,
+          Math.floor(progress * lines.length)
+        );
+        if (index !== activeIndex) {
+          activeIndex = index;
+          lines.forEach((line, i) => {
+            line.classList.toggle("is-active", i === index);
+          });
+        }
+      }
+    });
+   });
+}
+});
